58de4c0d5a27d5ba2b4560d880e2ebb3baed7fa9
ArjunPraveen <44439198+ArjunPraveen@users.noreply.github.com>
2020-08-27 01:51:17 +0530

    Good problem

---
+++ Graphs_DP/numberOfIslands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for(int i=0; i<grid.size(); i++)
+        {
+            for(int j=0; j<grid[i].size(); j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count+=1;
+                    bfs(grid, i,j);
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    void bfs(vector<vector<char>> &grid, int i, int j)
+    {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[i].size() || grid[i][j]=='0' )
+            return ;
+        grid[i][j]='0';
+        bfs(grid, i+1, j);
+        bfs(grid, i, j+1);
+        bfs(grid, i-1, j);
+        bfs(grid, i, j-1);
+    }
+};
