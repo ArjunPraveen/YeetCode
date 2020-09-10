0bf77374e2cb95904c227863bd8bd4db9775ffb4
ArjunPraveen <44439198+ArjunPraveen@users.noreply.github.com>
2020-09-11 01:15:25 +0530

    Create alternatingBits.cpp

---
+++ Random/alternatingBits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int one=0, zero=0, check=0;
+        if(n==0 or n==1)
+            return true;
+        while(n!=0)
+        {
+            check = n&1;
+            if(check==1)
+            {
+                if(one==1)
+                    return false;
+                one=1;
+                zero=0;
+            }
+            else
+            {
+                if(zero==1)
+                    return false;
+                zero=1;
+                one=0;
+            }
+            
+            n = n>>1;
+        }
+            
+            
+        return true;
+    }
+};
+
+
